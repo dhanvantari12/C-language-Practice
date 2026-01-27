65ac78ac0607b140a7e0c41fe8ab517d071d7a8a
dhanvantari12 <dhanvantariakolkar@gmail.com>
2026-01-27 22:10:57 +0530

    Add new program for Compound interest

---
+++ basics/cmpdInterest.c
@@ -0,0 +1,24 @@
+//Program to calculate Compound and simple Interest.
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    float principal, rate, time, compoundInterest, amount;
+
+    printf("Enter Principal amount: ");
+    scanf("%f", &principal);
+
+    printf("Enter Rate of Interest: ");
+    scanf("%f", &rate);
+
+    printf("Enter Time (in years): ");
+    scanf("%f", &time);
+
+    amount = principal * pow((1 + rate / 100), time);
+    compoundInterest = amount - principal;
+
+    printf("Compound Interest = %.2f\n", compoundInterest);
+    printf("Total Amount = %.2f\n", amount);
+
+    return 0;
+}
